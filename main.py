bb322beb5b58418decdbb1951b40f1ca9bcc3c50
Alan <alandavidg13@gmail.com>
2020-12-02 17:03:25 -0600

    pycharm!

--- main.py
+++ main.py
@@ -1,77 +1,66 @@
-#util
+# util
 import os
-import random
 import re
-import uuid
-
-#time
-from datetime import datetime, timedelta
 import time
+import uuid
+# time
+from datetime import datetime, timedelta
 
-#twitter
+# twitter
 import tweepy
-#utility
+
+# utility
 from util.tw_util import TwUtil
 
-
-#keys.
 key = os.environ.get('key')
 secret = os.environ.get('secret')
 token = os.environ.get('token')
 token_secret = os.environ.get('token_secret')
 
 
-
-
-def getApi():
-	auth = tweepy.OAuthHandler(key, secret)
-	auth.set_access_token(token, token_secret)
-	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-	return api
-
+def get_api():
+    auth = tweepy.OAuthHandler(key, secret)
+    auth.set_access_token(token, token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    return api
 
 
 def mentions():
-	api = getApi()
-	replies = 0
+    api = get_api()
+    replies = 0
 
-	for tweet in tweepy.Cursor(api.search, q="@"+api.me().screen_name).items(10):
-		now = datetime.utcnow()-timedelta(minutes=20)
-		if(now < tweet.created_at):
-			sendTweet(tweet.id_str)
-			replies += 1
-	return replies
+    for tweet in tweepy.Cursor(api.search, q="@" + api.me().screen_name).items(10):
+        now = datetime.utcnow() - timedelta(minutes=20)
+        if now < tweet.created_at:
+            send_tweet(tweet.id_str)
+            replies += 1
+    return replies
 
 
 def pms():
-	api = getApi()
+    api = get_api()
 
 
+def send_tweet(reply=None):
+    api = get_api()
+    text = TwUtil.get_text()
+    urls = re.findall('https?:.*?(?:\.png|\.jpg|\.gif)', text)
 
+    # data
+    images = TwUtil.get_images(urls)  # convert images to bytes.
+    text = TwUtil.clear_urls(text, urls)  # remove urls.
+    ids = [api.media_upload(filename=str(uuid.uuid4()) + ".png", file=im).media_id for im in images]
 
-def sendTweet(reply = None):
-	api = getApi()
-	text = TwUtil.getText()
-	urls = re.findall('https?:.*?(?:\.png|\.jpg|\.gif)', text)
+    # send
+    if reply is None:
+        api.update_status(status=text, media_ids=ids)
+    else:
+        api.update_status(status=text, media_ids=ids, in_reply_to_status_id=reply, auto_populate_reply_metadata=True)
 
-
-	#data
-	images = TwUtil.getImages(urls) #convert images to bytes.
-	text = TwUtil.clearUrls(text, urls) #remove urls.
-	ids = [api.media_upload(filename=str(uuid.uuid4())+".png", file=im).media_id for im in images] 
-
-
-	#send
-	if reply is None:
-	api.update_status(status = text, media_ids = ids)
-	else:
-	api.update_status(status = text, media_ids = ids, in_reply_to_status_id = reply, auto_populate_reply_metadata = True)
-
-	print("Tweet sent!")
-
+    print("Tweet sent!")
 
 
 while True:
-	sendTweet()
-	mentions()
-	time.sleep(60*10)
+    send_tweet()
+    mentions()
+    time.sleep(60 * 10)
--- util/tw_util.py
+++ util/tw_util.py
@@ -1,40 +1,37 @@
-import os 
+import os
 import random
 from os.path import dirname, abspath
-#images
+# images
 import requests
 from io import BytesIO
 
 
-
-
-
 class TwUtil:
 
-  def getText():
-    lines = open("./words.txt").read().splitlines()
-    sentence = ""
-    maxSentenceSize = random.randint(5, 140)
-    count = 0
-    for r in range(len(lines)):
-      text = random.choice(lines)
-      count += len(text)
-      if maxSentenceSize < count:
-        break
-      sentence = sentence+" "+text
-    return sentence
+    @staticmethod
+    def get_text():
+        lines = open("./words.txt").read().splitlines()
+        sentence = ""
+        max_word_size = random.randint(5, 140)
+        count = 0
+        for r in range(len(lines)):
+            text = random.choice(lines)
+            count += len(text)
+            if max_word_size < count:
+                break
+            sentence = sentence + " " + text
+        return sentence
 
+    @staticmethod
+    def get_images(urls):
+        images = []
+        for url in urls:
+            response = requests.get(url)
+            images.append(BytesIO(response.content))
+        return images
 
-  def getImages(urls):
-    images = []
-    for url in urls:
-      response = requests.get(url)
-      images.append(BytesIO(response.content))
-    return images
-
-
-
-  def clearUrls(sentence, urls):
-    for url in urls:
-      sentence = sentence.replace(url, '')
-    return sentence
+    @staticmethod
+    def clear_urls(sentence, urls):
+        for url in urls:
+            sentence = sentence.replace(url, '')
+        return sentence
